0eec42aca46dbbcc7fa368da0b90a4677438943b
Douglas Lima <doug.lb07@gmail.com>
2020-11-13 14:29:09 -0300

    feat: implementing action model

---
+++ src/main/java/br/com/fiap/ChappieApplication.java
@@ -0,0 +1,28 @@
+package br.com.fiap;
+
+import org.apache.catalina.Context;
+import org.apache.tomcat.util.scan.StandardJarScanner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class ChappieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChappieApplication.class, args);
+	}
+
+	@Bean
+	public TomcatServletWebServerFactory tomcatFactory() {
+		return new TomcatServletWebServerFactory() {
+			@Override
+			protected void postProcessContext(Context context) {
+				((StandardJarScanner) context.getJarScanner()).setScanManifest(false);
+			}
+		};
+	}
+
+
+}
