0f811479fb15966ee239ec87a9e286ca41a427dd
Douglas Lima <doug.lb07@gmail.com>
2020-11-17 00:14:18 -0300

    chore: updating execution repository

--- src/main/java/br/com/fiap/repository/ExecucaoRepository.java
+++ src/main/java/br/com/fiap/repository/ExecucaoRepository.java
@@ -1,8 +1,9 @@
 package br.com.fiap.repository;
 
+import br.com.fiap.model.ExecucaoModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ExecucaoRepository extends JpaRepository<ExecucaoRepository, Long> {
+public interface ExecucaoRepository extends JpaRepository<ExecucaoModel, Long> {
 }
