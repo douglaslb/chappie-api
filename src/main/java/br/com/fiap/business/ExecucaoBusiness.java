bab2ad06f0cd5e4b6e31ca9e004031787ed65e9b
Douglas Lima <doug.lb07@gmail.com>
2020-11-18 21:15:12 -0300

    feat: adding business rules

--- src/main/java/br/com/fiap/business/ExecucaoBusiness.java
+++ src/main/java/br/com/fiap/business/ExecucaoBusiness.java
@@ -9,6 +9,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.time.Instant;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
 import java.util.Optional;
 
 @Component
@@ -23,6 +29,10 @@ public class ExecucaoBusiness {
     public ExecucaoModel applyBusiness(ExecucaoModel execucao) throws ResponseBusinessException {
         verifyActionStatus(execucao.getAcao());
 
+        ZonedDateTime currentDate = getCurrentDate(execucao);
+
+        formatDateToString(currentDate, execucao);
+
         return execucao;
     }
 
@@ -30,10 +40,22 @@ public class ExecucaoBusiness {
 
         AcaoModel acao = acaoRepository.findById(acaoModel.getId()).get();
 
-        if(!acao.getAtivo()) {
+        if (!acao.getAtivo()) {
             throw new ResponseBusinessException(actionStatus);
         }
     }
 
+    protected ZonedDateTime getCurrentDate(ExecucaoModel execucao) throws ResponseBusinessException {
+
+        ZonedDateTime currentDate = ZonedDateTime.now();
+
+        return currentDate;
+    }
+
+    protected void formatDateToString(ZonedDateTime currentDate, ExecucaoModel execucao) throws ResponseBusinessException {
+        String formattedDate = DateTimeFormatter.ofPattern("dd/MM/yyyy - HH                                                                                                                                                                                                                                                                                                                                                                     :mm").format(currentDate);
+
+        execucao.setdataExecucao(formattedDate);
+    }
 
 }
